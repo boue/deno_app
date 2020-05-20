b22f3bd9996fb5b4bbaca945f11495e5b1ab587e
boue <boue.germain@gmail.com>
2020-05-19 19:15:55 -0700

    Create dummy products and a type Product interface

---
+++ controllers/products.ts
@@ -0,0 +1,23 @@
+import { Product } from "../types.ts";
+
+let products: Product[] = [
+  {
+    id: "1",
+    name: "Nitrogen",
+    description:
+      "Nitrogen is considered as a mineral element that is most demanded by plants, thus representing largest input in crop production management. It is the most difficult input to manage due to its changes in form, chemistry and locations.",
+    price: 30,
+  },
+  {
+    id: "2",
+    name: "Fertility Prescriptions",
+    description: "Prescription tools",
+    price: 299,
+  },
+  {
+    id: "3",
+    name: "Data Inbox",
+    description: "Import files",
+    price: 12,
+  },
+];
