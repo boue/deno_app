5f7cf89e8106bac42a589e4ac742170f08185ef9
boue <boue.germain@gmail.com>
2020-05-25 07:46:26 -0700

    Create the addProduct route that creates a uuid as well

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,3 +1,4 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import { Product } from "../types.ts";
 
 let products: Product[] = [
@@ -55,4 +56,29 @@ const getProduct = (
   }
 };
 
-export { getProducts, getProduct };
+// POST one product
+// http://localhost:5000/api/v1/products/
+const addProduct = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
+
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      msg: "No data",
+    };
+  } else {
+    const product: Product = body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
+};
+
+export { getProducts, getProduct, addProduct };
